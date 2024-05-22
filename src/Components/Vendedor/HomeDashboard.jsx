22c5c634de88e1ae0d734fda41d436cb6f3f1bbc
Nicole Lima <116369597+nicolelimam@users.noreply.github.com>
2024-05-22 17:59:05 -0300

    Adicionando menu na tela da empresa vendedora

--- src/Components/Vendedor/HomeDashboard.jsx
+++ src/Components/Vendedor/HomeDashboard.jsx
@@ -3,9 +3,27 @@ import './HomeDashboard.css';
 import { Helmet } from "react-helmet";
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faHouse, faBell, faHandHoldingDollar, faCartFlatbed, faSquarePollVertical, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import logo_img from '../Assets/icon-logotipo.svg';
+import { faChevronRight, faHouse, faBell, faTag, faCartFlatbed, faHandshake, faSquarePollVertical, faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 const HomeDashboard = () => {
+
+  useEffect(() => {
+    //Definindo as configurações do menu para expandir e diminuir
+    const body = document.querySelector("body");
+    const sidebar = body.querySelector(".sidebar");
+    const toggle = body.querySelector(".toggle");
+
+    const handleToggleClick = () => {
+      sidebar.classList.toggle("closed");
+    };
+
+    toggle.addEventListener("click", handleToggleClick);
+
+    return () => {
+      toggle.removeEventListener("click", handleToggleClick);
+    };
+  }, []);
 
   return (
       <div className="container">
@@ -13,58 +31,85 @@ const HomeDashboard = () => {
             <title>Home | Dashboard</title>
         </Helmet>
 
-        <nav className="sidebar">
+        <nav className="sidebar closed">
             <header>
                 <div className="panel-logo">
+                    <span className="icon-logo"><FontAwesomeIcon icon={faHandshake} className="icon-logotipo"/></span>
                     <span className="logotipo">Logo</span>
                 </div>
 
-                <FontAwesomeIcon icon={faChevronRight} className="toggle icon"/>
+                <div className="toggle">
+                    <FontAwesomeIcon icon={faChevronRight} className="toggle-icon"/>
+                </div>
             </header>
 
             <div className="menu-bar">
                 <div className="menu">
                     <ul className="nav-list">
                         <li className="nav-link">
-                            <a href="">
+                            <a href="" className="active" id="linkInicio">
                                 <FontAwesomeIcon icon={faHouse} className="nav-icon"/>
                                 <span className="text nav-text">Início</span>
                             </a>
+                            <span className="nav-tooltip">
+                                Início
+                            </span>
                         </li>
                         <li className="nav-link">
-                            <a href="">
+                            <a href="" id="linkNotificacoes">
                                 <FontAwesomeIcon icon={faBell} className="nav-icon"/>
                                 <span className="text nav-text">Notificações</span>
                             </a>
+                            <span className="nav-tooltip">
+                                Notificações
+                            </span>
                         </li>
                         <li className="nav-link">
-                            <a href="">
-                                <FontAwesomeIcon icon={faHandHoldingDollar} className="nav-icon" />
+                            <a href="" id="linkVendas">
+                                <FontAwesomeIcon icon={faTag} className="nav-icon"/>
                                 <span className="text nav-text">Vendas</span>
                             </a>
+                            <span className="nav-tooltip">
+                                Vendas
+                            </span>
                         </li>
                         <li className="nav-link">
-                            <a href="">
+                            <a href="" id="linkInventario">
                                 <FontAwesomeIcon icon={faCartFlatbed} className="nav-icon"/>
                                 <span className="text nav-text">Inventário</span>
                             </a>
+                            <span className="nav-tooltip">
+                                Inventário
+                            </span>
                         </li>
                         <li className="nav-link">
-                            <a href="">
+                            <a href="" id="linkRelatorio">
                                 <FontAwesomeIcon icon={faSquarePollVertical} className="nav-icon"/>
                                 <span className="text nav-text">Relatório</span>
                             </a>
-                        </li>
-                        <li className="nav-link btn-logout">
-                            <a href="">
-                                <FontAwesomeIcon icon={faRightFromBracket} className="nav-icon"/>
-                                <span className="text nav-text">Sair</span>
-                            </a>
+                            <span className="nav-tooltip">
+                                Relatório
+                            </span>
                         </li>
                     </ul>
                 </div>
+                <div className="content-bottom">
+                    <li className="btn-logout">
+                        <a href="" id="linkLogout">
+                            <FontAwesomeIcon icon={faRightFromBracket} className="nav-icon"/>
+                            <span className="text nav-text">Sair</span>
+                        </a>
+                        <span className="nav-tooltip tooltip-logout">
+                                Relatório
+                        </span>
+                    </li>
+                </div>
             </div>
         </nav>
+
+        <div className="main">
+            <div className="texto">Dashboard</div>
+        </div>
       </div>
   );
 };
